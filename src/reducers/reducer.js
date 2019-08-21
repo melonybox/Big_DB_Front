31947520b2899203e68cbae9ef72ec283b083d7d
melonybox <grandmadcow@gmail.com>
2019-08-20 20:12:25 -0400

    App.js fix, reducer fix

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,3 +1,11 @@
 const initialState = {
   loadedList: []
 }
+
+export default function reducer(state = initialState, action) {
+    console.log(action)
+    switch (action.type) {
+      default:
+        return state;
+    }
+  }
