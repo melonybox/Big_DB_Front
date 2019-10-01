2b23c54b906d31d64e1925f37143c43b8ca04523
melonybox <grandmadcow@gmail.com>
2019-10-01 13:38:20 -0400

    Added visual placement of where page numbers will be.

--- src/containers/listBox.js
+++ src/containers/listBox.js
@@ -3,10 +3,16 @@ import ListComponent from '../components/listComponent';
 
 class ListBox extends Component {
 
+
   render() {
     return (
       <div style={{display: "flex",justifyContent: "center",flexDirection: "column",alignItems: "center"}}>
         <ListComponent />
+          <div style={{display: "flex",flexDirection: "row"}}>
+            {[...Array(5)].map((e, i) => {
+              return <p key={i}>{i+1}</p>
+            })}
+          </div>
       </div>
     );
   }
