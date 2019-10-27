ca0d9d2b641abcf4365cb3fb306c1cf9d001ccaa
melonybox <grandmadcow@gmail.com>
2019-10-26 23:38:07 -0400

    Adventures in doing new page numbering

--- src/containers/listBox.js
+++ src/containers/listBox.js
@@ -6,9 +6,14 @@ import PageComponent from '../components/pageComponent';
 class ListBox extends Component {
 
   handleHi = () => {
+    let newArray = Array(5).map((e) => {
+      debugger
+      return e = this.props.currPage+1
+    })
+
     return <div style={{display: "flex",flexDirection: "row"}}>
-      {[...Array(this.props.totalPages)].map((e, i) => {
-        return <PageComponent key={i} pageNum={i+1} viewCount={this.props.viewCount} />
+      {[...newArray].map((e, i) => {
+        return <p>{e} | {i}</p>
       })}
     </div>
   }
@@ -31,7 +36,8 @@ class ListBox extends Component {
 
 const mapStateToProps = state => ({
   viewCount: state.viewCount,
-  totalPages: state.totalPages
+  totalPages: state.totalPages,
+  currPage: state.currPage
 })
 
 export default connect(mapStateToProps)(ListBox);
