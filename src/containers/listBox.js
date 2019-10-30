6d9d12e9f46ae38286b771e9ef667e286d933605
melonybox <grandmadcow@gmail.com>
2019-10-29 22:30:59 -0400

    Array logic fix

--- src/containers/listBox.js
+++ src/containers/listBox.js
@@ -15,7 +15,7 @@ class ListBox extends Component {
           return <PageComponent key={i} pageNum={i+1} viewCount={this.props.viewCount} />
         } else if (this.props.currPage+3 > this.props.totalPages && this.props.totalPages >= 5) {
           return <PageComponent key={i} pageNum={((this.props.totalPages-5)+i)+1} viewCount={this.props.viewCount} />
-        } else if (this.props.totalPages >= 5){
+        } else if (this.props.totalPages >= 5) {
           return <PageComponent key={i} pageNum={((this.props.currPage-2)+i)+1} viewCount={this.props.viewCount} />
         }
       })}
