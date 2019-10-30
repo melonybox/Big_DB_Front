b125e6bf94f7eba96c1a3abe26712654c2e45936
melonybox <grandmadcow@gmail.com>
2019-10-29 22:23:04 -0400

    Fixed page list number logic for edge case

--- src/containers/listBox.js
+++ src/containers/listBox.js
@@ -9,18 +9,18 @@ class ListBox extends Component {
 
     return <div style={{display: "flex",flexDirection: "row"}}>
       {[...Array(5)].map((e, i) => {
-        if (this.props.currPage < 3) {
+        if (this.props.totalPages < 5 && this.props.totalPages > i) {
           return <PageComponent key={i} pageNum={i+1} viewCount={this.props.viewCount} />
-        } else if (this.props.currPage+3 > this.props.totalPages) {
+        } else if (this.props.currPage < 3 && this.props.totalPages >= 5) {
+          return <PageComponent key={i} pageNum={i+1} viewCount={this.props.viewCount} />
+        } else if (this.props.currPage+3 > this.props.totalPages && this.props.totalPages >= 5) {
           return <PageComponent key={i} pageNum={((this.props.totalPages-5)+i)+1} viewCount={this.props.viewCount} />
-        } else {
+        } else if (this.props.totalPages >= 5){
           return <PageComponent key={i} pageNum={((this.props.currPage-2)+i)+1} viewCount={this.props.viewCount} />
-
         }
       })}
     </div>
   }
-
 
   render() {
     return (
