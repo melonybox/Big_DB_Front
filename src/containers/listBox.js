748e6e35a8f650f4093375aef929449f6b81428f
melonybox <grandmadcow@gmail.com>
2019-11-03 20:58:54 -0500

    Removed page list logic post converting it into ruby on back end so front end only has to display.

--- src/containers/listBox.js
+++ src/containers/listBox.js
@@ -5,31 +5,13 @@ import PageComponent from '../components/pageComponent';
 
 class ListBox extends Component {
 
-  handleHi = () => {
-
-    return <div style={{display: "flex",flexDirection: "row"}}>
-      {[...Array(5)].map((e, i) => {
-        if (this.props.totalPages < 5 && this.props.totalPages > i) {
-          return <PageComponent key={i} pageNum={i+1} viewCount={this.props.viewCount} />
-        } else if (this.props.currPage < 3 && this.props.totalPages >= 5) {
-          return <PageComponent key={i} pageNum={i+1} viewCount={this.props.viewCount} />
-        } else if (this.props.currPage+3 > this.props.totalPages && this.props.totalPages >= 5) {
-          return <PageComponent key={i} pageNum={((this.props.totalPages-5)+i)+1} viewCount={this.props.viewCount} />
-        } else if (this.props.totalPages >= 5) {
-          return <PageComponent key={i} pageNum={((this.props.currPage-2)+i)+1} viewCount={this.props.viewCount} />
-        }
-      })}
-    </div>
-  }
-
   render() {
     return (
       <div style={{display: "flex",justifyContent: "center",flexDirection: "column",alignItems: "center"}}>
         <ListComponent />
-          {this.handleHi()}
           <div style={{display: "flex",flexDirection: "row"}}>
-            {[...Array(this.props.totalPages)].map((e, i) => {
-              return <PageComponent key={i} pageNum={i+1} viewCount={this.props.viewCount} />
+            {this.props.pageList.map((i) => {
+              return <PageComponent key={i} pageNum={i} viewCount={this.props.viewCount} />
             })}
           </div>
       </div>
@@ -39,8 +21,7 @@ class ListBox extends Component {
 
 const mapStateToProps = state => ({
   viewCount: state.viewCount,
-  totalPages: state.totalPages,
-  currPage: state.currPage
+  pageList: state.pageList
 })
 
 export default connect(mapStateToProps)(ListBox);
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,14 +1,15 @@
 const initialState = {
   loadedList: [],
   totalPages: 0,
-  viewCount: 5,
-  currPage: 0
+  viewCount: 10,
+  currPage: 0,
+  pageList: []
 }
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
       case 'FILL_LIST':
-        return {...state, loadedList: action.payload[0], totalPages: action.payload[1], currPage: action.payload[2]}
+        return {...state, loadedList: action.payload[0], totalPages: action.payload[1], currPage: action.payload[2], pageList: action.payload[3]}
       case 'PAGE_NUM_CHANGE':
         return {...state, currPage: action.payload}
       default:
