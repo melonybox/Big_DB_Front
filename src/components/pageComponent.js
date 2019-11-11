4fb457298d6879ca04e25f4714c217376d3a484b
melonybox <grandmadcow@gmail.com>
2019-11-10 19:03:41 -0500

    Build Test

--- src/components/pageComponent.js
+++ src/components/pageComponent.js
@@ -4,8 +4,8 @@ import {getUsersAmountFetch} from '../actions/action';
 
 class PageComponent extends React.PureComponent {
 
-  handleClick = (amount,pageNum, event) => {
-    this.props.getUsersAmountFetch(amount,pageNum)
+  handleClick = (amount,pageNum,type,order, event) => {
+    this.props.getUsersAmountFetch(amount,pageNum,type,order)
   }
 
   render() {
@@ -14,17 +14,19 @@ class PageComponent extends React.PureComponent {
       ?
       <p style={{margin: "0.5rem"}}>{this.props.pageNum}</p>
       :
-      <p onClick={this.handleClick.bind(this, this.props.viewCount,(this.props.pageNum-1))} style={{cursor: "pointer", margin: "0.5rem", fontWeight: "bold"}}>{this.props.pageNum}</p>
+      <p onClick={this.handleClick.bind(this, this.props.viewCount,(this.props.pageNum-1),this.props.listSortType,this.props.listSortOrder)} style={{cursor: "pointer", margin: "0.5rem", fontWeight: "bold"}}>{this.props.pageNum}</p>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  currPage: state.currPage
+  currPage: state.currPage,
+  listSortType: state.listSortType,
+  listSortOrder: state.listSortOrder
 })
 
 const mapDispatchToProps = dispatch => ({
-  getUsersAmountFetch: (amount,pageNum) => dispatch(getUsersAmountFetch(amount,pageNum))
+  getUsersAmountFetch: (amount,pageNum,type,order) => dispatch(getUsersAmountFetch(amount,pageNum,type,order))
 })
 
 export default connect(mapStateToProps,mapDispatchToProps)(PageComponent);
--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import thunk from 'redux-thunk';
 
 const store = createStore(reducer, compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     ));
 
 ReactDOM.render(
